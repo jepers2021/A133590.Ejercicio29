1f821410abb91bc5236492ccdf8185acaee90977
Jorge <jepers2021@gmail.com>
2021-03-29 16:29:51 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace A133590.Ejercicio29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ejercicio 29");
+            Console.WriteLine("Programa que presenta el primero del mes inmediato anterior a la fecha ingresada.");
+
+            Console.Write("Por favor, ingrese una fecha: ");
+            DateTime fecha;
+            if (!DateTime.TryParse(Console.ReadLine(), out fecha))
+            {
+                Console.WriteLine("La fecha ingresada no es válida.");
+                Console.WriteLine("Presione cualquier tecla para continuar..");
+                Console.ReadKey();
+                return;
+            }
+
+            Console.WriteLine($"Resultado: {fecha.AddDays(-fecha.Day + 1).AddMonths(-1).ToString("dd/MM/yyyy")}");
+
+            Console.WriteLine("Presione cualquier tecla para continuar..");
+            Console.ReadKey();
+        }
+    }
+}
